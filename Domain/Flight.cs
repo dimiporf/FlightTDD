41ddf5fb3ad170eac6e7bb81a6074e35d98115b8
Dimitrios Porfiropoulos <dimiporf@live.com>
2023-12-28 19:34:14 +0100

    Added implementation of ContainEquivaltOf

--- Domain/Flight.cs
+++ Domain/Flight.cs
@@ -2,32 +2,47 @@
 {
     public class Flight
     {
+        public List<Booking> BookingList { get; set; } = new List<Booking>();
+
+        // The number of remaining seats on the flight
         public int RemainingNumberOfSeats { get; set; }
+
+        // Constructor to initialize the flight with a given seat capacity
         public Flight(int seatCapacity)
         {
             RemainingNumberOfSeats = seatCapacity;
         }
 
+        // Method to book a certain number of seats for a passenger
         public object? Book(string passengerEmail, int numberOfSeats)
         {
+            // Check if there are enough remaining seats to accommodate the booking
             if (numberOfSeats > this.RemainingNumberOfSeats)
             {
+                // Return an OverbookingError if there are not enough seats
                 return new OverbookingError();
             }
 
+            // Reduce the number of remaining seats based on the booking
             RemainingNumberOfSeats -= numberOfSeats;
 
-            //Devil's Advocate example #3
-            //if (numberOfSeats == 1) RemainingNumberOfSeats = 2;
-            //if (numberOfSeats == 3) RemainingNumberOfSeats = 3;
+            BookingList.Add(new Booking(passengerEmail, numberOfSeats));
 
-            //Devil's Advocate example #2
-            //RemainingNumberOfSeats = 2;
+            // Devil's Advocate examples (commented out for normal functionality)
+            // Uncomment these lines to simulate alternative scenarios for testing
 
+            // Devil's Advocate example #1
+            // return new OverbookingError();
+
+            // Devil's Advocate example #2
+            // RemainingNumberOfSeats = 2;
+
+            // Devil's Advocate example #3
+            // if (numberOfSeats == 1) RemainingNumberOfSeats = 2;
+            // if (numberOfSeats == 3) RemainingNumberOfSeats = 3;
+
+            // Return null indicating a successful booking
             return null;
-
-            //Devil's Advocate example #1
-            //return new OverbookingError();
         }
     }
 }
--- FlightTDD/FlightSpecifications.cs
+++ FlightTDD/FlightSpecifications.cs
@@ -5,72 +5,76 @@ namespace FlightTDD
 {
     public class FlightSpecifications
     {
+        // Parameterized test to verify that booking reduces the number of seats correctly
         [Theory]
         [InlineData(3, 1, 2)]
         [InlineData(6, 3, 3)]
         [InlineData(10, 6, 4)]
         public void Booking_reduces_the_number_of_seats_Parameterized(int seatCapacity, int numberOfSeats, int remainingNumberOfSeats)
         {
+            // Arrange: Create a flight with the specified seat capacity
             var flight = new Flight(seatCapacity: seatCapacity);
 
+            // Act: Book a specific number of seats
             flight.Book("dimiporf@live.com", numberOfSeats);
 
+            // Assert: Check that the remaining number of seats matches the expected value
             flight.RemainingNumberOfSeats.Should().Be(remainingNumberOfSeats);
-
         }
 
+        // Test to verify that booking reduces the number of seats
         [Fact]
         public void Booking_reduces_the_number_of_seats()
         {
+            // Arrange: Create a flight with a seat capacity of 3
             var flight = new Flight(seatCapacity: 3);
 
+            // Act: Book 1 seat
             flight.Book("dimiporf@live.com", 1);
 
+            // Assert: Check that the remaining number of seats is 2
             flight.RemainingNumberOfSeats.Should().Be(2);
-
         }
 
-        [Fact]
-        public void Booking_reduces_the_number_of_seats_2()
-        {
-            var flight = new Flight(seatCapacity: 6);
+        // Similar tests for different scenarios...
 
-            flight.Book("dimiporf@live.com", 3);
-
-            flight.RemainingNumberOfSeats.Should().Be(3);
-
-        }
-
-        [Fact]
-        public void Booking_reduces_the_number_of_seats_3()
-        {
-            var flight = new Flight(seatCapacity: 10);
-
-            flight.Book("dimiporf@live.com", 6);
-
-            flight.RemainingNumberOfSeats.Should().Be(4);
-
-        }
-
+        // Test to verify that overbooking is avoided
         [Fact]
         public void Avoids_overbooking()
         {
-            //Given
+            // Given: Create a flight with a seat capacity of 3
             var flight = new Flight(seatCapacity: 3);
 
-            //When
+            // When: Attempt to book more seats than the capacity
             var error = flight.Book("dimiporf@live.com", 4);
 
-            //Then
+            // Then: Check that an OverbookingError is returned
             error.Should().BeOfType<OverbookingError>();
         }
 
+        // Test to verify that flights are booked successfully
         [Fact]
         public void Books_flights_successfully()
         {
+            // Arrange: Create a flight with a seat capacity of 3
             var flight = new Flight(seatCapacity: 3);
+
+            // Act: Attempt to book 1 seat
             var error = flight.Book("dimiporf@live.com", 1);
+
+            // Assert: Check that no error is returned (booking successful)
             error.Should().BeNull();
+        }
+
+
+        [Fact]
+        public void Remembers_bookings()
+        {
+            var flight = new Flight(seatCapacity: 150);
+
+            flight.Book(passengerEmail: "dimiporf@live.com", numberOfSeats: 4);
+
+            flight.BookingList.Should().ContainEquivalentOf(new Booking("dimiporf@live.com", 4));
         }
     }
 }
